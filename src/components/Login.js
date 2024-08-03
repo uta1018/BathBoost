02fbd696a1bc592c330551f69c144c7bafdb8947
maiyu-u <ayumayou007@yahoo.ne.jp>
2024-08-03 22:24:51 +0900

    ログイン画面のCSS整備

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import { auth, db, provider } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../providers/Provider";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import "./css/Login.css";
 
 const Login = () => {
   const { setIsAuth } = useContext(Context);
@@ -32,9 +33,25 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <p>ログインして始める</p>
-      <button onClick={logInWithGoogle}>Googleでログイン</button>
+    <div className="login-container">
+      <div className="header">
+        <div className="logo-box">
+          <div className="logo">BATH BOOST</div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="image-container">
+          <div className="circle"></div>
+          <img src="../img/login_img.png" alt="Animal" className="login_image" />
+        </div>
+
+
+        <div className="title">BATH BOOST</div>
+        <button className="login-button">Googleアカウントでログイン</button>
+      </div>
+      <div className="footer">
+        <p>ライラック</p>
+      </div>
     </div>
   );
 };
