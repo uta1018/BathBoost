bc1836ad8e841281a92f86bfb09b4b874e79c373
shimmen <y.shin3141@gmail.com>
2024-08-04 14:44:57 +0900

    ルーム情報画面をスタイリング

--- src/components/RoomInfo.js
+++ src/components/RoomInfo.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from '../providers/Provider';
+import "./css/RoomInfo.css";
 
 const RoomInfo = () => {
   const { roomID } = useContext(Context);
@@ -14,14 +15,28 @@ const RoomInfo = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <p>ルームIDは{roomID}です</p>
+    <div className="roominfo-container">
+      <div className="header">
+        <div className="logo-box">
+          <img src="/logo.png" className="logoImg"></img>
+          <div className="logo">BATH BOOST</div>
+        </div>
       </div>
-      <p>友達にルームIDをシェアしよう!</p>
-      <Link to="/room">ルームへ</Link>
+      <div className="content">
+        <div className="speech-bubble">
+          <p>ルームIDは</p>
+          <h1>{roomID}</h1>
+          <p>です</p>
+        </div>
+        <img src="/tellID.png" className="tell-id-img" />
+        <p>友達にリンク、ルームIDをシェアしよう！</p>
+      <Link to="/room" className="room-link">ルームへ</Link>
+      </div>
+      <div className="footer">
+        <p>@ライラック</p>
+      </div>
     </div>
   )
-}
+};
 
 export default RoomInfo
