711dba9a536f566259c9e95f38c32c82a8ab3418
maiyu-u <ayumayou007@yahoo.ne.jp>
2024-08-04 14:48:25 +0900

    時間選択画面のスタイリング

--- src/components/TimeSelect.js
+++ src/components/TimeSelect.js
@@ -3,6 +3,7 @@ import { Context } from "../providers/Provider";
 import { auth, db } from "../firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import './css/TimeSelect.css';
 
 const toLocalISOString = (date) => {
   const localDate = new Date(date - date.getTimezoneOffset() * 60000);
@@ -41,14 +42,28 @@ const TimeSelect = () => {
   };
 
   return (
-    <div>
-      <h3>何時にお風呂入る?</h3>
-      <input
-        type="datetime-local"
-        value={goalTime}
-        onChange={(e) => setGoalTime(e.target.value)}
-      />
-      <button onClick={postData}>決定</button>
+    <div class="TimeSelect_container">
+      <div className="header">
+        <div className="logo-box">
+          <img src="/logo.png" className="logoImg"></img>
+          <div className="logo">BATH BOOST</div>
+        </div>
+      </div>
+      <div class="timeSelect">
+        <h3>何時に入る?</h3>
+        <input
+          type="datetime-local"
+          value={goalTime}
+          onChange={(e) => setGoalTime(e.target.value)}
+        />
+        <div class="button-container">
+          <button onClick={postData}>決定</button>
+        </div>
+      </div>
+      <img src='TimeQuestion.png' className="image" alt="Time Question"/>
+      <div className="footer">
+        <h2>@ライラック</h2>
+      </div>
     </div>
   );
 };
