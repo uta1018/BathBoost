902eb1a40a2888f12a67689a9fdfcda4d6c26bfa
Utaho Akashi <akashi.uta.1212@gmail.com>
2024-08-24 22:51:12 +0900

    add:RoomDetailのDB操作実装 #17

--- src/components/common/RoomDetail.js
+++ src/components/common/RoomDetail.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-const RoomDetail = ({ closeRoomDetail, id, roomName }) => {
+const RoomDetail = ({
+  closeRoomDetail,
+  id,
+  roomName,
+  member,
+  author,
+  date,
+}) => {
   return (
     <div>
       <p>Room Name: {roomName}</p>
       <p>Room ID: {id}</p>
+      <div>
+        <p>メンバー:</p>
+        {member.map((user, index) => (
+          <span key={user.userID}>
+            <img src={user.icon} alt="" />
+            {user.userName}
+            {index < member.length - 1 && ", "}
+          </span>
+        ))}
+      </div>
+      <p>作成日: {date}</p>
+      <p>作成者: {author.userName}</p>
       <button onClick={closeRoomDetail}>とじる</button>
     </div>
   );
