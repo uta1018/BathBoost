91698f43469176c7799d6fda538da7f450f40350
shimmen <y.shin3141@gmail.com>
2024-08-24 17:59:52 +0900

    Log.jsを追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import User from "./components/user/User";
+import Log from "./components/log/Log";
 import Login from "./components/login/Login";
 import Room from "./components/room/Room";
 import RoomInfo from "./components/room/RoomInfo";
@@ -16,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/user" element={<User />}></Route>
-          {/* <Route path="/log" element={<Log />}></Route> */}
+          <Route path="/log" element={<Log />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/room" element={<Room />}></Route>
           <Route path="/roominfo" element={<RoomInfo />}></Route>
---
+++ src/components/log/Log.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../common/Navbar';
+
+const Log = () => {
+    return (
+        <div>
+            <div>ログ画面</div>
+            <Navbar currentPage="log" />
+        </div>
+    );
+}
+
+export default Log;
