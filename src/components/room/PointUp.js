b8917ea661d0e15ded00ca7e1978ca3d7bb10c12
Utaho Akashi <akashi.uta.1212@gmail.com>
2024-08-25 09:58:51 +0900

    add:画像ファイルの追加・ポイントアップポップアップに要素配置 #34

--- src/components/room/PointUp.js
+++ src/components/room/PointUp.js
@@ -1,20 +1,33 @@
 import React from "react";
+import PopupHeader from "../common/PopupHeader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const PointUp = ({ nextPoint, point, closePointUp, removeOverlay }) => {
   console.log("ポイントUPポップアップ");
 
   return (
     <div className="popup-content">
+      <PopupHeader title="おふろポイント" />
       {point > 0 ? (
         <div>
-          <p>おふろポイントUP</p>
+          <p>おふろポイントUP!</p>
+          <p>
+            <FontAwesomeIcon icon={faPlus} />
+            {point}pt
+          </p>
+          <img src="/pointUp/cat_nice.png" alt="すごい!!" width="100px" />
         </div>
       ) : (
         <div>
           <p>おふろポイントDOWN</p>
+          <p>
+            <FontAwesomeIcon icon={faMinus} />
+            1pt
+          </p>
+          <img src="/pointUp/frog_sad.png" alt="残念…" width="100px" />
         </div>
       )}
-      <p>{point}pt</p>
       <p>つぎのレベルまであと{nextPoint}pt</p>
       <button
         onClick={() => {
