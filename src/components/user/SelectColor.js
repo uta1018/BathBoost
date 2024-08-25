7fdb13a266ea681fe35b7bc886bbe0ba9f12a1c9
Utaho Akashi <akashi.uta.1212@gmail.com>
2024-08-26 00:53:12 +0900

    add:SelectColorを実装 #50

--- src/components/user/SelectColor.js
+++ src/components/user/SelectColor.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { memo, useContext, useEffect, useState } from "react";
+import { Context } from "../../providers/Provider";
 
-const SelectColor = () => {
+const SelectColor = memo(({ themeColor, themeColorList }) => {
+  const { userID } = useContext(Context);
+  // 選択されたアイコンを保存する変数
+  const [selectedColor, setSelectedColor] = useState(themeColor);
+
+  console.log("SelectColor");
+
+  useEffect(() => {
+    setSelectedColor(themeColor);
+  }, [themeColor]);
+
   return (
-    <div>SelectColor</div>
-  )
-}
+    <div>
+      {themeColorList &&
+        themeColorList.map((color, index) => {
+          return (
+            <img
+              key={index}
+              src={`/themeColor/${color}.png`}
+              alt="テーマカラー"
+              onClick={() => setSelectedColor(color)}
+              style={{
+                width: "40px",
+                borderRadius: "100%",
+                outlineOffset: "3px",
+                outline: selectedColor === color ? "3px solid #B9B9B9" : "",
+              }}
+            />
+          );
+        })}
+    </div>
+  );
+});
 
-export default SelectColor
+export default SelectColor;
