a3e8ccd8936bf852c58c17af78961e254ee0da3c
shimmen <y.shin3141@gmail.com>
2024-08-28 16:31:55 +0900

    add:PopupHeader.jsに肉球アイコンを追加 #18

--- src/components/common/PopupHeader.js
+++ src/components/common/PopupHeader.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+
 // ポップアップのタイトルを受け取って表示
 const PopupHeader = ({ title }) => {
-  return <div>{title}</div>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faPaw} />
+      {title}
+    </div>
+  );
 };
 
 export default PopupHeader;
