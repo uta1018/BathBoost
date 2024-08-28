33e99796dd26b32f894b28da1510abcb14c8d719
maiyu-u <ayumayou007@yahoo.ne.jp>
2024-08-28 14:59:20 +0900

    update: Navbarにアイコンを追加

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+//アイコンのインポート
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faFilePen } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = ({ currentPage }) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // ボタンの色を変更する関数
+  const buttonStyle = (page) => {
+    // 現在のページと一致する場合、背景色を変更
+    return currentPage === page ? { backgroundColor: "#97D0BE" } : {};
+  };
 
-    // ボタンの色を変更する関数
-    const buttonStyle = (page) => {
-        // 現在のページと一致する場合、背景色を変更
-        return currentPage === page ? { backgroundColor: '#97D0BE' } : {};
-    };
+  return (
+    <nav>
+      {/* ユーザーボタン */}
+      <button onClick={() => navigate("/user")}>
+        <FontAwesomeIcon icon={faGear} color="red"/>
+        せってい
+      </button>
 
-    return (
-        <nav>
-            {/* ユーザーボタン */}
-            <button onClick={() => navigate("/user")} >
-                ユーザー
-            </button>
+      {/* ホームボタン */}
+      <button onClick={() => navigate("/")}>
+        <FontAwesomeIcon icon={faHouse} color="red"/>
+        ホーム
+      </button>
 
-            {/* ホームボタン */}
-            <button onClick={() => navigate("/")} >
-                ホーム
-            </button>
-
-            {/* ログボタン */}
-            <button onClick={() => navigate("/log")}>
-                ログ
-            </button>
-        </nav>
-    );
+      {/* ログボタン */}
+      <button onClick={() => navigate("/log")}>
+        <FontAwesomeIcon icon={faFilePen} color="red"/>
+        きろく
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
