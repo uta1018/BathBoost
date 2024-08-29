b462d8fa1e74d020021a5b0e90aadfdb55ed5c40
shimmen <y.shin3141@gmail.com>
2024-08-29 19:27:50 +0900

    add:記録画面の仮実装 #41

--- src/components/log/Log.js
+++ src/components/log/Log.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Navbar from '../common/Navbar';
+import PageHeader from "../common/PageHeader";
+import PageSubheading from "../common/PageSubheading";
+import Help from "../common/Help";
+
+// アイコンのインポート
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePen } from "@fortawesome/free-solid-svg-icons";
 
 const Log = () => {
     return (
+      <div>
+        <PageHeader title="きろく" />
+        <Help />
         <div>
-            <div>ログ画面</div>
-            <Navbar currentPage="log" />
+          <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+          おふろレベル Lv.17
         </div>
+        <div>
+          <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+          おふろポイント 167pt
+        </div>
+        <PageSubheading title="れんぞくおふろきろく" />
+        <div>現在　最長</div>
+        <div>4回　17回</div>
+        <Navbar currentPage="log" />
+      </div>
     );
 }
 
