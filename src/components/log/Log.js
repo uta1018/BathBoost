0470c05bbd9075a6ef0d9e9244747c956f4360ef
shimmen <y.shin3141@gmail.com>
2024-08-29 20:39:48 +0900

    update:記録画面にDBを実装 #45

--- src/components/log/Log.js
+++ src/components/log/Log.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import Navbar from '../common/Navbar';
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../providers/Provider";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../common/Navbar";
 import PageHeader from "../common/PageHeader";
 import PageSubheading from "../common/PageSubheading";
 import Help from "../common/Help";
@@ -9,24 +13,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePen } from "@fortawesome/free-solid-svg-icons";
 
 const Log = () => {
-    return (
+
+    const { userID } = useContext(Context);
+    const [userData, setUserData] = useState(null);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      console.log("ユーザーデータを取得");
+      const fetchData = async () => {
+        // ログインしていなかったらログイン画面へ
+        if (!userID) {
+          navigate("/login");
+          return;
+        }
+
+        // ユーザーデータを取得
+        const userDocRef = doc(db, "user", userID);
+        const userDocSnap = await getDoc(userDocRef);
+
+        if (userDocSnap.exists()) {
+          setUserData({ id: userDocSnap.id, ...userDocSnap.data() });
+        } else {
+          console.log("ユーザーデータが見つかりません");
+        }
+      };
+
+      fetchData();
+    }, [userID, navigate]);
+
+    if (!userData) {
+      return <div>Loading...</div>;
+    }
+
+  return (
+    <div>
+      <PageHeader title="きろく" />
+      <Help />
       <div>
-        <PageHeader title="きろく" />
-        <Help />
-        <div>
-          <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
-          おふろレベル Lv.17
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
-          おふろポイント 167pt
-        </div>
-        <PageSubheading title="れんぞくおふろきろく" />
-        <div>現在　最長</div>
-        <div>4回　17回</div>
-        <Navbar currentPage="log" />
+        <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+        おふろレベル Lv.{userData.level}
       </div>
-    );
-}
+      <div>
+        <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+        おふろポイント {userData.point}pt
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+        おふろ回数 {userData.bathCount}回
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faFilePen} color={"#FFD4BC"} />
+        登録から {userData.date}日
+      </div>
+      <PageSubheading title="れんぞくおふろきろく" />
+      <div>現在　最長</div>
+      <div>
+        {userData.goalStreakCount}回　{userData.longestGoalStreakCount}回
+      </div>
+      <Navbar currentPage="log" />
+    </div>
+  );
+};
 
 export default Log;
