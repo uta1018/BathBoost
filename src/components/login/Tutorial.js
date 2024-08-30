d05b28f9d942dc3437c9d13c13e4a7b53240fc66
shimmen <y.shin3141@gmail.com>
2024-08-30 19:17:58 +0900

    add:チュートリアルにカルーセルの実装 #54

--- src/components/login/Tutorial.js
+++ src/components/login/Tutorial.js
@@ -1,13 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import PopupHeader from "../common/PopupHeader";
 
-const Tutorial = ({closeHelp, showCloseButton=false}) => {
-  return (
+const Tutorial = ({ closeHelp, showCloseButton = false }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // チュートリアルページを要素として定義
+  const pages = [
     <div>
-      Tutorial
-      {/* ボタンを押したときチュートリアルを閉じる */}
-      {showCloseButton &&<button onClick={closeHelp}>閉じる</button>}
-    </div>
-  )
-}
+      <div>ルームを作ってルームIDを友達にシェアしよう！</div>
+      <div>ホームのルーム作成ボタンを押すと新しいルームを作れるよ</div>
+    </div>,
+    <div>
+      <div>2ページ目</div>
+    </div>,
+    <div>
+      <div>3ページ目</div>
+    </div>,
+    <div>
+      <div>4ページ目</div>
+    </div>,
+    <div>
+      <div>5ページ目</div>
+    </div>,
+  ];
 
-export default Tutorial
+  const handleNext = () => {
+    if (currentIndex < pages.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  return (
+    <div className="tutorial-popup">
+      <PopupHeader title={`使い方 (${currentIndex + 1}/${pages.length})`} />
+
+      {/* 現在のページを表示 */}
+      {pages[currentIndex]}
+
+      <div className="tutorial-popup-controls">
+        <button onClick={handlePrev} disabled={currentIndex === 0}>
+          &lt;
+        </button>
+        <button
+          onClick={handleNext}
+          disabled={currentIndex === pages.length - 1}
+        >
+          &gt;
+        </button>
+      </div>
+
+      {/* ボタンを押したときチュートリアルを閉じる */}
+      {showCloseButton && <button onClick={closeHelp}>とじる</button>}
+    </div>
+  );
+};
+
+export default Tutorial;
