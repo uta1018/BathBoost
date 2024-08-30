b1ee2adc8a856f260fb4ee921708776aec173db3
shimmen <y.shin3141@gmail.com>
2024-08-30 19:39:21 +0900

    update:Login画面とHelpでチュートリアルの挙動を変更 #54

--- src/components/login/Tutorial.js
+++ src/components/login/Tutorial.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PopupHeader from "../common/PopupHeader";
+import "../css/Popup.css";
 
 const Tutorial = ({ closeHelp, showCloseButton = false }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -7,8 +8,7 @@ const Tutorial = ({ closeHelp, showCloseButton = false }) => {
   // チュートリアルページを要素として定義
   const pages = [
     <div>
-      <div>ルームを作ってルームIDを友達にシェアしよう！</div>
-      <div>ホームのルーム作成ボタンを押すと新しいルームを作れるよ</div>
+      <div>1ページ目</div>
     </div>,
     <div>
       <div>2ページ目</div>
@@ -27,32 +27,34 @@ const Tutorial = ({ closeHelp, showCloseButton = false }) => {
   const handleNext = () => {
     if (currentIndex < pages.length - 1) {
       setCurrentIndex(currentIndex + 1);
+    } else {
+      // 最後のページから最初のページへ移動
+      setCurrentIndex(0);
     }
   };
 
   const handlePrev = () => {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
+    } else {
+      // 最初のページから最後のページへ移動
+      setCurrentIndex(pages.length - 1);
     }
   };
 
+  // classNameの適用を条件付きで行う
+  const tutorialContent = showCloseButton ? "popup-content" : "";
+
   return (
-    <div className="tutorial-popup">
+    <div className={tutorialContent}>
       <PopupHeader title={`使い方 (${currentIndex + 1}/${pages.length})`} />
 
       {/* 現在のページを表示 */}
       {pages[currentIndex]}
 
-      <div className="tutorial-popup-controls">
-        <button onClick={handlePrev} disabled={currentIndex === 0}>
-          &lt;
-        </button>
-        <button
-          onClick={handleNext}
-          disabled={currentIndex === pages.length - 1}
-        >
-          &gt;
-        </button>
+      <div>
+        <button onClick={handlePrev}>&lt;</button>
+        <button onClick={handleNext}>&gt;</button>
       </div>
 
       {/* ボタンを押したときチュートリアルを閉じる */}
