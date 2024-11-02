ffe9aac9b8d7d332292e258f5e62305bf4eca45b
maiyu-u <ayumayou007@yahoo.ne.jp>
2024-11-02 19:20:41 +0900

    update:post.favoriteUserの追加

--- src/components/room/PostItem.js
+++ src/components/room/PostItem.js
@@ -1,5 +1,8 @@
-import React, { memo, useContext } from "react";
+import React, { memo, useContext, useState } from "react";
 import { Context } from "../../providers/Provider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartsolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 
 // 時間表示を○○：○○にする関数
 const formatHHMM = (time) => {
@@ -25,6 +28,54 @@ const formatHHMMforTimeStamp = (timestamp) => {
   const minutes = date.getMinutes();
 
   return `${hours}:${minutes.toString().padStart(2, "0")}`;
+};
+
+// いいね表示する関数
+const FavoriteEndBath = ({ userID, post, initialFavorites }) => {
+  const [favorites, setFavorites] = useState(initialFavorites);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [showFavoriteUsers, setShowFavoriteUsers] = useState(false);
+  const [favoriteUser, setFavoriteUser] = useState(post.favoriteUser);
+  
+  const onClickFavorite = () => {
+    // if (userID === post.author) return;
+    if (isFavorite) {
+      setFavorites(favorites - 1);
+    } else {
+      setFavorites(favorites + 1);
+    }
+    setIsFavorite(!isFavorite);
+    setFavoriteUser(...favoriteUser, userID);
+  };
+
+  const onClickNumber = () => {
+    // いいねしたユーザーをポップアップ表示
+    setShowFavoriteUsers(!showFavoriteUsers);
+  };
+
+  return (
+    <div className="favorite-container" onClick={onClickFavorite}>
+      <FontAwesomeIcon
+        icon={isFavorite ? faHeartsolid : faHeartRegular}
+        color={isFavorite ? "var(--red)" : "var(--gray-300)"}
+        style={{ cursor: "pointer" }}
+      />
+      {favorites > 0 && <span onClick={onClickNumber}>{favorites}</span>}{" "}
+      {/* いいねの数が1以上のとき表示 */}
+      {/* ポップアップでユーザーリストを表示 */}
+      {showFavoriteUsers && (
+        <div className="favorite-users-popup">
+          {post.favoriteUser.map((user) => (
+            <div key={user.userID}>
+              <img src={user.icon} alt="" width="40px" />
+              <p>{user.userName}</p>
+            </div>
+          ))}
+          <button onClick={() => setShowFavoriteUsers(false)}>閉じる</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 const PostItem = memo(({ post, authorUser, previousPostDate }) => {
@@ -62,13 +113,22 @@ const PostItem = memo(({ post, authorUser, previousPostDate }) => {
         </div>
         <div className="stamp-wrapper">
           <p>{authorName}</p>
-          {post.type === "setBathGoal" && (
+          {post.type === "setBathGoal" ? (
             <div className="stamp-set-bath-goal">
               <h3 className="goalTime">{goalTime}</h3>
               <img src={post.stamp} alt="スタンプ" />
             </div>
-          )}
-          {post.type !== "setBathGoal" && (
+          ) : post.type === "endBath" ? (
+            <div className="stamp">
+              <img src={post.stamp} />
+              {/* いいね表示 */}
+              <FavoriteEndBath
+                userID={userID}
+                post={post}
+                initialFavorites={0} //post.favoriteにしたい
+              />
+            </div>
+          ) : (
             <div className="stamp">
               <img src={post.stamp} />
             </div>
