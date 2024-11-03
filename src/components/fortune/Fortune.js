8022223e4e2315ccf2eb3139cec5683df35431de
shimmen3141 <y.shin3141@gmail.com>
2024-11-03 15:14:44 +0900

    add:Fortune.jsにナビバーを設置 #90

--- src/components/fortune/Fortune.js
+++ src/components/fortune/Fortune.js
@@ -1,9 +1,11 @@
 import PageHeader from "../common/PageHeader";
+import Navbar from "../common/Navbar";
 
 const Fortune = () => {
   return (
     <div>
       <PageHeader title="うらない" />
+      <Navbar currentPage="fortune" />
     </div>
   );
 };
