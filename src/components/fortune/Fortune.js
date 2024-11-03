b3633b606eb3aea9de888bd961946669840bf4a0
shimmen3141 <y.shin3141@gmail.com>
2024-11-03 21:10:36 +0900

    update:うらない画面にボタンを追加 #94

--- src/components/fortune/Fortune.js
+++ src/components/fortune/Fortune.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import PageHeader from "../common/PageHeader";
 import Navbar from "../common/Navbar";
 
 const Fortune = () => {
+  const [showExtraButtons, setShowExtraButtons] = useState(false);
+
+  const handleShowExtraButtons = () => {
+    setShowExtraButtons(true);
+  };
+
+  const handleGoBack = () => {
+    setShowExtraButtons(false);
+  };
+
   return (
     <div>
       <PageHeader title="うらない" />
+      <div style={{ position: "relative", top: "100px" }}>
+        {!showExtraButtons ? (
+          // 最初のボタン
+          <button onClick={handleShowExtraButtons}>交換する</button>
+        ) : (
+          // 追加の3つのボタンと戻るボタン
+          <div>
+            <button onClick={handleGoBack}>戻る</button>
+            <div>
+              <button>ボタン1</button>
+              <button>ボタン2</button>
+              <button>ボタン3</button>
+            </div>
+          </div>
+        )}
+      </div>
       <Navbar currentPage="fortune" />
     </div>
   );
