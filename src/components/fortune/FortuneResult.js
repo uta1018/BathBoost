6774d028223dfe1af0cb7b06cb81c1dcf5e7cee2
shimmen3141 <y.shin3141@gmail.com>
2024-11-03 22:03:16 +0900

    update:ランダムなページ遷移を実装 #94

--- src/components/fortune/FortuneResult.js
+++ src/components/fortune/FortuneResult.js
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const FortuneResult = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
+
   return (
     <div>
       <h2>Fortune Result {id}</h2>
+      <button onClick={() => navigate("/fortune")}>戻る</button>
     </div>
   );
 };
