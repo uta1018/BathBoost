027569a63d0807f41ce43bed829ab32d2170344e
Utaho Akashi <akashi.uta.1212@gmail.com>
2024-11-04 15:02:47 +0900

    add:アニメーションの追加 #94

--- src/components/fortune/Fortune.js
+++ src/components/fortune/Fortune.js
@@ -1,16 +1,10 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PageHeader from "../common/PageHeader";
 import Navbar from "../common/Navbar";
 
 const Fortune = () => {
   const fortuneResultCount = 5;
-  const [showChoices, setShowChoices] = useState(false);
   const navigate = useNavigate();
-
-  const handleShowChoices = () => {
-    setShowChoices((prev) => !prev);
-  };
 
   const handleRandomNavigate = () => {
     // 0 から 4 までのランダムな整数
@@ -22,20 +16,7 @@ const Fortune = () => {
     <div>
       <PageHeader title="うらない" />
       <div style={{ position: "relative", top: "100px" }}>
-        {!showChoices ? (
-          // 最初のボタン
-          <button onClick={handleShowChoices}>交換する</button>
-        ) : (
-          // 追加の3つのボタンと戻るボタン
-          <div>
-            <button onClick={handleShowChoices}>戻る</button>
-            <div>
-              <button onClick={handleRandomNavigate}>ボタン1</button>
-              <button onClick={handleRandomNavigate}>ボタン2</button>
-              <button onClick={handleRandomNavigate}>ボタン3</button>
-            </div>
-          </div>
-        )}
+        <button onClick={handleRandomNavigate}>交換する</button>
       </div>
       <Navbar currentPage="fortune" />
     </div>
--- src/components/fortune/FortuneResult.js
+++ src/components/fortune/FortuneResult.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const FortuneResult = () => {
+  const [animation, setAnimation] = useState(0);
   const navigate = useNavigate();
   const location = useLocation();
   const { id } = useParams();
@@ -16,7 +17,86 @@ const FortuneResult = () => {
   return (
     <div>
       <h2>Fortune Result {id}</h2>
-      <button onClick={() => navigate("/fortune")}>戻る</button>
+      {animation == 0 && (
+        <div>
+          <img
+            src="/fortune/ice.png"
+            alt="アイス"
+            width={100}
+            onClick={() => {
+              setAnimation(1);
+            }}
+          />
+          <img
+            src="/fortune/ice.png"
+            alt="アイス"
+            width={100}
+            onClick={() => {
+              setAnimation(1);
+            }}
+          />
+          <img
+            src="/fortune/ice.png"
+            alt="アイス"
+            width={100}
+            onClick={() => {
+              setAnimation(1);
+            }}
+          />
+          <p>タップして選んでね</p>
+        </div>
+      )}
+      {animation == 1 && (
+        <div>
+          <img
+            src="/fortune/eating_ice1.png"
+            alt="食べかけのアイス"
+            width={300}
+            onClick={() => {
+              setAnimation(2);
+            }}
+          />
+          <p>タップしてね</p>
+        </div>
+      )}
+      {animation == 2 && (
+        <div>
+          <img
+            src="/fortune/eating_ice2.png"
+            alt="食べかけのアイス"
+            width={300}
+            onClick={() => {
+              setAnimation(3);
+            }}
+          />
+          <p>タップしてね</p>
+        </div>
+      )}
+      {animation == 3 && (
+        <div>
+          <img
+            src="/fortune/eating_ice3.png"
+            alt="食べかけのアイス"
+            width={300}
+            onClick={() => {
+              setAnimation(4);
+            }}
+          />
+          <p>タップしてね</p>
+        </div>
+      )}
+      {animation == 4 && id == 1 && (
+        <div>
+          <img
+            src="/fortune/result_1.png"
+            alt="大吉のアイスの棒"
+            width={300}
+            onClick={() => {
+              setAnimation(5);
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
